56cab65749d44c3af9aeecc0b1c1135322349ddb
up <up@example.com>
2021-12-18 15:57:24 +0800

    recog time

---
+++ app/src/main/java/com/zltd/ocr/FlleUtils.java
@@ -0,0 +1,210 @@
+package com.zltd.ocr;
+
+import android.annotation.SuppressLint;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class FlleUtils {
+    /**
+     * 获取目录下所有文件(按时间排序)
+     *
+     * @param path
+     * @return
+     */
+    public static List<File> listFileSortByModifyTime(String path) {
+        List<File> list = getFiles(path, new ArrayList<File>());
+        if (list != null && list.size() > 0) {
+            Collections.sort(list, new Comparator<File>() {
+                public int compare(File file, File newFile) {
+                    if (file.lastModified() > newFile.lastModified()) {
+                        return -1;
+                    } else if (file.lastModified() == newFile.lastModified()) {
+                        return 0;
+                    } else {
+                        return 1;
+                    }
+                }
+            });
+        }
+        return list;
+    }
+    public static boolean isImageFile(String filePath) {
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeFile(filePath, options);
+
+        if (options.outWidth == -1) {
+
+            return false;
+
+        }
+
+        return true;
+
+    }
+    /**
+     *
+     * 获取目录下所有文件
+     *
+     * @param realpath
+     * @param files
+     * @return
+     */
+    public static List<File> getFiles(String realpath, List<File> files) {
+        File realFile = new File(realpath);
+        if (realFile.isDirectory()) {
+            File[] subfiles = realFile.listFiles();
+            for (File file : subfiles) {
+                if (file.isDirectory()) {
+                    getFiles(file.getAbsolutePath(), files);
+                } else {
+                    files.add(file);
+                }
+            }
+        }
+        return files;
+    }
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        String res = null;
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+        if(null!=cursor&&cursor.moveToFirst()){;
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            res = cursor.getString(column_index);
+            cursor.close();
+        }
+        return res;
+    }
+
+    /**
+     * 专为Android4.4设计的从Uri获取文件绝对路径，以前的方法已不好使
+     */
+    @SuppressLint("NewApi")
+    public String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{split[1]};
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public String getDataColumn(Context context, Uri uri, String selection,
+                                String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {column};
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+}
--- app/src/main/java/com/zltd/ocr/MainActivity.java
+++ app/src/main/java/com/zltd/ocr/MainActivity.java
@@ -1,36 +1,192 @@
 package com.zltd.ocr;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.DocumentsContract;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 import com.zltd.ocr.databinding.ActivityMainBinding;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'ocr' library on application startup.
-    static {
-        System.loadLibrary("ocr");
-    }
+
     private String TAG = "bds";
     private ActivityMainBinding binding;
+
+    private FlleUtils mFlleUtils;
+    private Bitmap bitmap;
+    private TextView tv;
+    private String path = null;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    public static void verifyStoragePermissions(Activity activity) {
+        //权限检查
+        if (ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //申请权限
+            ActivityCompat.requestPermissions(activity,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+        //权限检查
+        if (ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //申请权限
+            ActivityCompat.requestPermissions(activity,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        Button reg = findViewById(R.id.recog);
+        verifyStoragePermissions(MainActivity.this);
+        mFlleUtils = new FlleUtils();
+        findViewById(R.id.bt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("content://com.android.externalstorage.documents/document/primary:Pictures/Screenshots");
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, uri);
+                startActivityForResult(intent, 1);
+            }
+        });
+        Log.d(TAG, "model=" + Build.MODEL);
+        String dir = null;
+        if (Build.MODEL.equals("MI 6X")) {
+            dir = "sdcard/DCIM/Screenshots";
+        } else {
+            dir = "sdcard/Pictures/Screenshots";
+        }
+        List<File> list = mFlleUtils.listFileSortByModifyTime(dir);
+        for (File file : list) {
+            if (mFlleUtils.isImageFile(file.getPath())) {
+                path = file.getPath();
+                Log.d(TAG, "fisrt file=" + path);
+                break;
+            }
+        }
+        if (path != null) {
+            reg.setVisibility(View.VISIBLE);
+            reg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    bitmap = BitmapFactory.decodeFile(path);
+                    Log.d(TAG, "wi=" + bitmap.getWidth());
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            detectText(bitmap);
+                        }
+                    }).start();
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
+                }
+            });
+        } else {
+            reg.setVisibility(View.GONE);
+        }
+//        String filePath = "/storage/emulated/0/at/Screenshot_20211213_091406_com.foreverht.newland.workplus.jpg";
+//        bitmap = BitmapFactory.decodeFile(filePath);
+//        Log.d(TAG, "wi=" + bitmap.getWidth());
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                detectText(bitmap);
+//            }
+//        }).start();
+
 
     }
-    public String detectText(Bitmap bitmap) {
+
+    private BitmapFactory.Options getBitmapOption(int inSampleSize) {
+        System.gc();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPurgeable = true;
+        options.inSampleSize = inSampleSize;
+        return options;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String path = null;
+        if (resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            if ("file".equalsIgnoreCase(uri.getScheme())) {//使用第三方应用打开
+                path = uri.getPath();
+                return;
+            }
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {//4.4以后
+                path = mFlleUtils.getPath(this, uri);
+            } else {//4.4以下下系统调用方法
+                path = mFlleUtils.getRealPathFromURI(this, uri);
+            }
+        }
+        if (path != null) {
+            Log.d(TAG, "path=" + path);
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(path);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            bitmap = BitmapFactory.decodeStream(fis);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    detectText(bitmap);
+                }
+            }).start();
+
+        }
+    }
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            String result = (String) msg.obj;
+            tv.setText(result);
+            Toast.makeText(MainActivity.this, "result=" + result, Toast.LENGTH_LONG).show();
+        }
+    };
+
+
+    public void detectText(Bitmap bitmap) {
         Log.d(TAG, "Initialization of TessBaseApi");
         TessDataManager.initTessTrainedData(MainActivity.this);
         TessBaseAPI tessBaseAPI = new TessBaseAPI();
@@ -39,19 +195,34 @@ public class MainActivity extends AppCompatActivity {
         tessBaseAPI.setDebug(true);
         tessBaseAPI.init(path, "eng");
         // 白名单
-        tessBaseAPI.setVariable(TessBaseAPI.VAR_CHAR_WHITELIST, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+        tessBaseAPI.setVariable(TessBaseAPI.VAR_CHAR_WHITELIST, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-:");
         // 黑名单
-        tessBaseAPI.setVariable(TessBaseAPI.VAR_CHAR_BLACKLIST, "!@#$%^&*()_+=-[]}{;:'\"\\|~`,./<>?");
+        tessBaseAPI.setVariable(TessBaseAPI.VAR_CHAR_BLACKLIST, "!@#$%^&*()_+=[]}{;'\"\\|~`,./<>?");
         tessBaseAPI.setPageSegMode(TessBaseAPI.PageSegMode.PSM_AUTO_OSD);
         Log.d(TAG, "Ended initialization of TessEngine");
         Log.d(TAG, "Running inspection on bitmap");
         tessBaseAPI.setImage(bitmap);
-        String inspection = tessBaseAPI.getHOCRText(0);
-
-        Log.d(TAG, "Confidence values: " + tessBaseAPI.meanConfidence());
+        String result = tessBaseAPI.getUTF8Text();
+        String lines[] = result.split("\\r?\\n");
+        int summary = 0;
+        for (String str : lines) {
+            Log.d(TAG, "result values= " + str);
+            if (TextUtils.isEmpty(str) || !str.contains(":") || !str.contains(" ")) continue;
+            if (str.startsWith("09")) {
+                String tmp[] = str.trim().split(" ");
+                String time[] = tmp[0].trim().split(":");
+                if (TextUtils.isDigitsOnly(time[1])) {
+                    int min = Integer.valueOf(time[1]);
+                    summary += min;
+                    Log.d(TAG, "result values= " + summary);
+                }
+            }
+        }
         tessBaseAPI.end();
         System.gc();
-        return inspection;
+        Message msg = new Message();
+        msg.obj = String.valueOf(summary);
+        mHandler.sendMessage(msg);
     }
 
     /**
